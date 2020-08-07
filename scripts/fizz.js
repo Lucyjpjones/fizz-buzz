ecd356e96f931e5ecfb78fc3d2b517abd699aa8c
Lucyjpjones <lucyjpjones@gmail.com>
2020-08-07 10:06:06 +0000

    fizz.js amended after reviewing hint

--- scripts/fizz.js
+++ scripts/fizz.js
@@ -1,12 +1,12 @@
 function fizzBuzz(number) {
     
-    if (number%3 && number%5) {
+    if (number%3 === 0 && number%5 === 0) {
         return "FizzBuzz";
     }
-    else if (number%3) {
+    else if (number%3 === 0) {
         return "Fizz";
     }
-    else if (number%5) {
+    else if (number%5 === 0) {
         return "Buzz";
     }
     else {
